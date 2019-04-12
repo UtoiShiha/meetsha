150cbe86ed41e81b2246bc04ccc92311af2484ae
UtoiShiha <ameken_shiha@yahoo.co.jp>
2019-04-13 00:02:26 +0900

    router enabled

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>Hello,Vue</h1>
+    <p v-if="msg.length > 0">
+      {{msg}}
+    </p>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'about',
+    data(){
+      return{
+        msg: 'Hellor Vue.js!'
+      }
+    },
+    methods:{
+    clear(){
+      this.msg = ''
+    }
+  }
+}
+</script>
